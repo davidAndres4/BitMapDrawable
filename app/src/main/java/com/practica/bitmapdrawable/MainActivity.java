bb3bcb237756e19329526da4331fd0c640acc7ce
David <david.andres.otero@iesjulianmarias.es>
2021-11-12 14:15:50 +0100

    muestra imagen con BitMapDrawable

---
+++ app/src/main/java/com/practica/bitmapdrawable/MainActivity.java
@@ -0,0 +1,57 @@
+package com.practica.bitmapdrawable;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //creamos una Activity y le asociamos un objeto View, creado en el constructor de la
+        //clase DibujaImagen
+        setContentView(new DibujaImagen(this));
+    }
+
+    //clase que hereda de View
+    public class DibujaImagen extends View {
+        //declaramos el objeto drawable
+        private BitmapDrawable bojack;
+
+        //constructor. Establece la vista mediante el contexto recibido de la actividad
+        //que lo invoca
+        public DibujaImagen(Context context) {
+            super(context);
+
+            //asociamos a nuestro objeto el id de recurso de la imagen almacenada en R.drawable
+            bojack = (BitmapDrawable) ContextCompat.getDrawable(context, R.drawable.mi_imagen);
+            //Con este método, indicamos el rectángulo donde ha de ser dibujado el objeto,
+            //es decir, establecemos sus límites
+            bojack.setBounds(30,30,200,200);
+        }
+
+        //sobreescribimos el método onDraw de la clase View, que será el responsable de
+        //dibujar la vista mediante un objeto de la clase Canvas que recibe como parámetro
+        @Override
+        protected void onDraw(Canvas canvas) {
+            //dibujamos la imagen
+            bojack.draw(canvas);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
